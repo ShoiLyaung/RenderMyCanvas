4a6d4718fbb0e9bd159d7fe5fd70b7e11ab699b0
Jonathan Zhang <admin@cestlavie.moe>
2024-10-14 18:35:10 +0800

    feat: Ellipse

--- RenderMyCanvas/src/Primitives/Ellipse.h
+++ RenderMyCanvas/src/Primitives/Ellipse.h
@@ -1,21 +1,26 @@
+// Ellipse.h
 #pragma once
 
 #include <glm/glm.hpp>
 #include "Primitive.h"
 
 namespace RMC {
+
     class Ellipse : public Primitive2D {
     public:
-		Ellipse() = default;
-        Ellipse(const glm::vec3& F1, const glm::vec3& F2);
+        Ellipse(const glm::vec3& center, float majorAxisLength, float minorAxisLength, float rotationAngle = 0.0f);
+
+        Ellipse(const glm::vec3& focus1, const glm::vec3& focus2, float majorAxisLength);
+
         void Draw(uint32_t* imageData, uint32_t width, uint32_t height) const override;
+
     protected:
         glm::vec3 Center;
-        glm::vec3 F1;
-        glm::vec3 F2;
-        float MajorAxis;
-        float MinorAxis;
-        float StartAngle;
-        float EndAngle;
+        float MajorAxisLength;
+        float MinorAxisLength;
+        glm::vec3 Focus1;
+        glm::vec3 Focus2;
+        float RotationAngle;
     };
+
 } // namespace RMC

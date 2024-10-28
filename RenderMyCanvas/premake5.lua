2fac95a897ec5733b59ee8466d83f8b5e5811dfd
ShoiLyaung <2596985510@qq.com>
2024-10-28 16:05:54 +0800

    Optimize premake5 for CUDA and LibTorch

--- RenderMyCanvas/premake5.lua
+++ RenderMyCanvas/premake5.lua
@@ -11,12 +11,7 @@ project "RenderMyCanvas"
    files {
       "src/**.h",
       "src/**.cpp",
-      "src/**.cuh", "src/**.cu",
-      "shaders/**.rgen",
-      "shaders/**.rchit",
-      "shaders/**.rmiss",
-      "shaders/**.rahit",
-      "shaders/**.rint",
+      "src/**.cuh", "src/**.cu"
    }
    includedirs
    {
@@ -67,25 +62,6 @@ project "RenderMyCanvas"
       defines { "WL_PLATFORM_WINDOWS" }
       defines { "TORCH_USE_CUDA" }
 
-   vpaths {
-      ["Shaders"] = { "shaders/**.rgen", "shaders/**.rchit", "shaders/**.rmiss", "shaders/**.rahit", "shaders/**.rint" },
-   }
-
-   -- 设置着色器文件的处理方式为“None”
-   filter { "files:**.rgen or files:**.rchit or files:**.rmiss or files:**.rahit or files:**.rint" }
-      buildaction "None"
-
-      -- 定义着色器编译的输出目录
-      shaderOutputDir = "%{cfg.targetdir}/shaders/compiled"
-
-      -- 为着色器文件添加自定义构建步骤
-      buildcommands {
-         "{MKDIR} \"" .. shaderOutputDir .. "\"",
-         "glslangValidator -V \"%{file.relpath}\" -o \"" .. shaderOutputDir .. "/%{file.basename}.spv\""
-      }
-      buildoutputs { shaderOutputDir .. "/%{file.basename}.spv" }
-      buildmessage "Compiling shader %{file.name}"
-
    filter "configurations:Debug"
       defines { "WL_DEBUG" }
       runtime "Debug"
@@ -103,3 +79,10 @@ project "RenderMyCanvas"
       runtime "Release"
       optimize "On"
       symbols "Off"
+
+   filter { "action:vs*", "files:**.cu" }
+      buildmessage 'Compiling CUDA source %{file.relpath}'
+      buildcommands { 
+          '"%{cudaPath}/bin/nvcc.exe" -gencode arch=compute_61,code=sm_61 -x cu -c "%{file.relpath}" -o "%{cfg.objdir}/%{file.basename}.obj" -I"%{cudaIncludeDir}"' 
+      }
+      buildoutputs { "%{cfg.objdir}/%{file.basename}.obj" }
